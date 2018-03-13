eb67926f235cbab5263f32892ff2480f17de3cc8
Daniel Brüggemann <github@barfooz.com>
2018-03-13 10:19:32 +0100

    Fix eslint errors and warnings in background.js

--- extension/background.js
+++ extension/background.js
@@ -1,16 +1,15 @@
 /* global console, browser, setTimeout, clearTimeout */
 /* eslint new-cap: off */
-"use strict";
 
 // Max memory usage: (200 + 200) * 500 bytes = 200 kB; less in practice.
 // Plus information about the waiting tabs, and data structure overhead.
-var LRU_CACHE_SIZE = 200;
-var LRU_WAITING_SIZE = 200;
-var MAX_URL_SIZE = 500;
-var QUEUE_MAX_TIME = 2000;
+const LRU_CACHE_SIZE = 200;
+const LRU_WAITING_SIZE = 200;
+const MAX_URL_SIZE = 500;
+const QUEUE_MAX_TIME = 2000;
 
-var DefaultAnimationBehavior = null;
-var CurrentAnimationBehavior = null;
+let DefaultAnimationBehavior = null;
+let CurrentAnimationBehavior = null;
 
 function isDataUrl(url) {
   return url.startsWith("data:");
@@ -21,13 +20,13 @@ function invertBehavior(value) {
 }
 
 function notifyAnimated(who, url) {
-  let msg = { type: "notify-animated", url };
+  const msg = { type: "notify-animated", url };
   browser.tabs
     .sendMessage(who.tabId, msg, { frameId: who.frameId })
     .catch(e => console.error(e));
 }
 
-var AnimatedUrlCache = {
+const AnimatedUrlCache = {
   entries: new Set(),
   waiting: new Map(),
 
@@ -42,24 +41,26 @@ var AnimatedUrlCache = {
       if (this.entries.length > LRU_CACHE_SIZE)
         this.evict(this.entries, (LRU_CACHE_SIZE * 3) >> 2);
 
-      let w = this.waiting.get(url);
+      const w = this.waiting.get(url);
       if (w) {
         this.waiting.delete(url);
-        for (let who of w) notifyAnimated(who, url);
+        w.forEach(who => notifyAnimated(who, url));
       }
     }
   },
 
+  // TODO: @simonlindholm are you sure this code needs to be so complicated?
   evict(map, keep) {
-    var ind = 0,
-      del = map.length - keep,
-      ar = [],
-      key;
-    for (key of map.keys()) {
-      if (++ind > del) break;
-      ar.push(key);
-    }
-    for (key of ar) map.delete(key);
+    const del = map.length - keep;
+    const ar = [];
+    let ind = 0;
+
+    map.keys().forEach(key => {
+      ind += 1;
+      if (ind <= del) ar.push(key);
+    });
+
+    ar.forEach(key => map.delete(key));
   },
 
   checkAnimated(who, url) {
@@ -67,24 +68,22 @@ var AnimatedUrlCache = {
       // TODO! (Either data URLs are not passed through webRequest, in
       // which case we need to do GIF parsing here or in the child
       // process, or they are, and we need to crop them.)
+    } else if (this.entries.has(url)) {
+      // Maintain LRU order.
+      this.entries.delete(url);
+      this.entries.add(url);
+      notifyAnimated(who, url);
     } else {
-      if (this.entries.has(url)) {
-        // Maintain LRU order.
-        this.entries.delete(url);
-        this.entries.add(url);
-        notifyAnimated(who, url);
-      } else {
-        let w = this.waiting.get(url);
-        if (w) w.push(who);
-        else this.waiting.set(url, [who]);
-        if (this.waiting.length > LRU_WAITING_SIZE)
-          this.evict(this.waiting, (LRU_WAITING_SIZE * 3) >> 2);
-      }
+      const w = this.waiting.get(url);
+      if (w) w.push(who);
+      else this.waiting.set(url, [who]);
+      if (this.waiting.length > LRU_WAITING_SIZE)
+        this.evict(this.waiting, (LRU_WAITING_SIZE * 3) >> 2);
     }
   }
 };
 
-var TemporaryQueue = {
+const TemporaryQueue = {
   state: "default", // default | changed | transition
   // TODO: save temporary setting
 
@@ -96,16 +95,16 @@ var TemporaryQueue = {
   blockers: new Map(),
 
   push(wanted, key, callback) {
-    let tp = wanted === DefaultAnimationBehavior ? "default" : "changed";
+    const tp = wanted === DefaultAnimationBehavior ? "default" : "changed";
     this.queues[tp].push({ key, callback });
     this.work();
   },
 
   work() {
     if (this.state === "transition") return;
-    for (let item of this.queues[this.state]) {
-      let key = item.key;
-      var timeout = setTimeout(() => {
+    this.queues[this.state].forEach(item => {
+      const { key } = item;
+      const timeout = setTimeout(() => {
         // Revert the change after a timeout, e.g. if the tab gets
         // closed at just the wrong moment.
         console.warn("Reverting animation mode change after timeout");
@@ -119,16 +118,16 @@ var TemporaryQueue = {
         // Not our or the caller's fault if this throws an exception!
         console.error(ex);
       }
-    }
+    });
     this.queues[this.state] = [];
 
     if (
       this.blockers.size === 0 &&
       (this.state === "changed" || this.queues.changed.length > 0)
     ) {
-      let newState = this.state === "changed" ? "default" : "changed";
+      const newState = this.state === "changed" ? "default" : "changed";
       this.state = "transition";
-      let behavior =
+      const behavior =
         newState === "default"
           ? DefaultAnimationBehavior
           : invertBehavior(DefaultAnimationBehavior);
@@ -143,7 +142,7 @@ var TemporaryQueue = {
   },
 
   done(key) {
-    let entry = this.blockers.get(key);
+    const entry = this.blockers.get(key);
     if (entry) {
       this.blockers.delete(key);
       clearTimeout(entry.timeout);
@@ -153,11 +152,13 @@ var TemporaryQueue = {
 };
 
 function* gifDecoder($) {
-  var size, r, len;
+  let size;
+  let r;
+  let len;
 
   // Header
   if ($.avail < 6) yield $.Ensure(6);
-  var header = $.read(6);
+  const header = $.read(6);
   if (header[0] !== 0x47 || header[1] !== 0x49 || header[2] !== 0x46)
     return $.Error("not a gif");
   if (
@@ -170,7 +171,7 @@ function* gifDecoder($) {
   // Metadata
   if ($.avail < 7) yield $.Ensure(7);
   $.skip(4); // size
-  var bits = $.read1(); // has gct, color resolution, sorted, gct size
+  let bits = $.read1(); // has gct, color resolution, sorted, gct size
   $.skip(2); // background color, aspect ratio
 
   // GCT
@@ -180,10 +181,10 @@ function* gifDecoder($) {
     if (r) yield r;
   }
 
-  var atLeastOneImage = false;
+  let atLeastOneImage = false;
   for (;;) {
     if ($.avail < 1) yield $.Ensure(1);
-    var type = $.read1();
+    let type = $.read1();
 
     // Extension block
     if (type === 0x21) {
@@ -206,15 +207,15 @@ function* gifDecoder($) {
         len = $.read1();
       } else if (type === 0xff && len === 11) {
         if ($.avail < 12) yield $.Ensure(12);
-        var ext = $.read(11),
-          exts = "";
-        for (var i = 0; i < 11; i++) exts += String.fromCharCode(ext[i]);
+        const ext = $.read(11);
+        let exts = "";
+        for (let i = 0; i < 11; i++) exts += String.fromCharCode(ext[i]);
         len = $.read1();
         if (exts === "NETSCAPE2.0" || exts === "ANIMEXTS1.0") {
           while (len) {
             len = Math.max(len, 3);
             if ($.avail < len + 1) yield $.Ensure(len + 1);
-            var subid = $.read1() & 0x7;
+            const subid = $.read1() & 0x7;
             if (subid === 1) {
               $.readU16(); // iteration count, 0 = inf
             } else if (subid !== 2) {
@@ -277,8 +278,9 @@ function* gifDecoder($) {
   return $.Done();
 }
 
-function setFilterHandlers(filter, url, decoder, foundAnimation) {
-  var $ = {
+function setFilterHandlers(filterHandler, url, decoder, foundAnimation) {
+  const filter = filterHandler;
+  const $ = {
     avail: 0,
     index: 0,
     skipping: 0,
@@ -301,8 +303,11 @@ function setFilterHandlers(filter, url, decoder, foundAnimation) {
       return { type: 3 };
     },
 
-    feed(arr) {
+    feed(feedArr) {
+      let arr = feedArr;
+
       if (!arr.length) return;
+
       if (this.skipping) {
         if (this.skipping >= arr.length) {
           this.skipping -= arr.length;
@@ -317,7 +322,7 @@ function setFilterHandlers(filter, url, decoder, foundAnimation) {
           if (this.avail !== 0)
             this.leftovers = this.bytearray.subarray(this.index);
         } else {
-          var buffer = new Uint8Array(new ArrayBuffer(this.avail));
+          const buffer = new Uint8Array(new ArrayBuffer(this.avail));
           buffer.set(this.leftovers);
           buffer.set(
             this.bytearray.subarray(this.index),
@@ -332,21 +337,22 @@ function setFilterHandlers(filter, url, decoder, foundAnimation) {
     },
 
     longSkip(n) {
-      if (this.avail < n) {
-        n -= this.avail;
+      let toSkip = n;
+      if (this.avail < toSkip) {
+        toSkip -= this.avail;
         this.avail = 0;
         this.index = 0;
         this.leftovers = null;
         this.bytearray = null;
-        this.skipping = n;
-        return this.Ensure(n);
+        this.skipping = toSkip;
+        return this.Ensure(toSkip);
       }
-      $.skip(n);
+      $.skip(toSkip);
       return null;
     },
 
     read(n) {
-      var ret;
+      let ret;
       this.avail -= n;
       if (this.avail < 0) {
         throw new Error("must ensure space before reading!");
@@ -393,14 +399,14 @@ function setFilterHandlers(filter, url, decoder, foundAnimation) {
     },
 
     read1() {
-      var ret;
+      let ret;
       this.avail -= 1;
       if (this.avail < 0) {
         throw new Error("must ensure space before reading!");
       }
 
       if (this.leftovers !== null) {
-        ret = this.leftovers[0];
+        [ret] = this.leftovers;
         this.leftovers =
           this.leftovers.length === 1 ? null : this.leftovers.subarray(1);
       } else {
@@ -411,14 +417,14 @@ function setFilterHandlers(filter, url, decoder, foundAnimation) {
     },
 
     readU16() {
-      var a = this.read1();
-      var b = this.read1();
+      const a = this.read1();
+      const b = this.read1();
       return a | (b << 8);
     }
   };
 
-  var dec = decoder($);
-  var waitingFor = 0;
+  const dec = decoder($);
+  let waitingFor = 0;
 
   function done() {
     filter.disconnect();
@@ -435,14 +441,14 @@ function setFilterHandlers(filter, url, decoder, foundAnimation) {
 
     $.feed(arr);
     while ($.avail >= waitingFor) {
-      var r = dec.next();
-      var s = r.value;
+      const r = dec.next();
+      const s = r.value;
       if (s.type === 0) {
         waitingFor = s.n;
         if ($.avail >= waitingFor)
           throw new Error("yielded despite being able to continue");
       } else if (s.type === 1) {
-        console.log("error in image decode: " + s.msg + " on " + url);
+        console.info(`error in image decode: ${s.msg} on ${url}`);
         done();
         return;
       } else if (s.type === 2) {
@@ -454,7 +460,7 @@ function setFilterHandlers(filter, url, decoder, foundAnimation) {
         done();
         return;
       } else {
-        throw new Error("unknown event type " + s.type);
+        throw new Error(`unknown event type ${s.type}`);
       }
     }
   };
@@ -472,23 +478,26 @@ function isGif(req) {
     // but that adds more overhead.)
     return true;
   }
-  for (let header of req.responseHeaders) {
-    if (header.name.toLowerCase() === "content-type")
-      return header.value === "image/gif";
-  }
+
+  req.responseHeaders.forEach(
+    header =>
+      header.name.toLowerCase() === "content-type" &&
+      header.value === "image/gif"
+  );
+
   return false;
 }
 
 function requestListener(req) {
-  var status = req.statusCode;
-  if (200 <= status && status < 300 && isGif(req)) {
-    var url = req.url;
+  const status = req.statusCode;
+  if (status >= 200 && status < 300 && isGif(req)) {
+    const { url } = req;
     // XXX Does req.url take redirects into account, or do I need to hook
     // onBeforeRedirect as well? (And check requestId's. With expiry on
     // onErrorOccurred.)
-    let filter = browser.webRequest.filterResponseData(req.requestId);
+    const filter = browser.webRequest.filterResponseData(req.requestId);
     setFilterHandlers(filter, url, gifDecoder, () => {
-      console.log("found animated image " + url);
+      console.info(`found animated image ${url}`);
       AnimatedUrlCache.insert(url);
     });
   }
@@ -501,7 +510,7 @@ browser.webRequest.onHeadersReceived.addListener(
   ["blocking", "responseHeaders"]
 );
 
-let animationBehaviorPromise = browser.browserSettings.imageAnimationBehavior
+const animationBehaviorPromise = browser.browserSettings.imageAnimationBehavior
   .get({})
   .then(({ value }) => {
     if (DefaultAnimationBehavior === null) {
@@ -510,12 +519,12 @@ let animationBehaviorPromise = browser.browserSettings.imageAnimationBehavior
     }
   })
   .catch(e => {
-    throw new Error("unable to read image animation behavior: " + String(e));
+    throw new Error(`unable to read image animation behavior: ${String(e)}`);
   });
 
 browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.type === "query-animated") {
-    let who = { tabId: sender.tab.id, frameId: sender.frameId };
+    const who = { tabId: sender.tab.id, frameId: sender.frameId };
     AnimatedUrlCache.checkAnimated(who, msg.url);
   } else if (msg.type === "query-animation-behavior") {
     if (CurrentAnimationBehavior === null) {
@@ -528,8 +537,8 @@ browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   } else if (msg.type === "temporary-behavior") {
     if (DefaultAnimationBehavior === null)
       throw new Error("content-script must init before changing behavior");
-    var key = msg.key,
-      wanted = msg.value;
+    const { key } = msg;
+    const wanted = msg.value;
     TemporaryQueue.push(wanted, key, () => {
       sendResponse();
     });
@@ -537,7 +546,7 @@ browser.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   } else if (msg.type === "done-temporary-behavior") {
     TemporaryQueue.done(msg.key);
   } else if (msg.type === "updated-pref") {
-    console.log("updated pref", msg.pref, msg.value);
+    console.info("updated pref", msg.pref, msg.value);
     if (msg.pref === "animation-behavior") {
       DefaultAnimationBehavior = msg.value;
       CurrentAnimationBehavior = msg.value;
